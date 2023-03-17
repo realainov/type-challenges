30af54a8ad5d6bb97c322942b073bc8b0b20d349
Mamaev Yuriy <mamaev@embria.com>
2023-03-17 08:33:13 +0300

    implements updating answer is-any

--- 223-is-any.ts
+++ 223-is-any.ts
@@ -2,19 +2,19 @@
   223 - IsAny
   -------
   by Pavel Glushkov (@pashutk) #hard #utils
-  
+
   ### Question
-  
+
   Sometimes it's useful to detect if you have a value with `any` type. This is especially helpful while working with third-party Typescript modules, which can `any` values in the module API. It's also good to know about `any` when you're suppressing implicitAny checks.
-  
+
   So, let's write a utility type `IsAny<T>`, which takes input type `T`. If `T` is `any`, return `true`, otherwise, return `false`.
-  
+
   > View on GitHub: https://tsch.js.org/223
 */
 
 /* _____________ Your Code Here _____________ */
 
-type IsAny<T> = Equal<T & 0, any> extends true ? true : false;
+type IsAny<T> = 0 extends T & 1 ? true : false;
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils';
